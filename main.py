16cc800b8acc3a7404d0ea782c6ae6f4633ed72d
Mateusz Groblicki <mateuszgroblicki@gmail.com>
2022-11-13 22:09:35 +0100

    code cleanup - config.py changes
    + moved part of the constants from config.py to related client classes in main.py
    
    + a few minor improvements

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ log.basicConfig(level=cfg.Logging.LEVEL,
 class Cache:
     """
     Add new key/value: 
-        cache[key] = value, timeout_seconds 
+        cache[key] = value, timeout_seconds
+        >>> cache = Cache()
         >>> cache['k1'] = 'value', 5
 
     Get value: 
@@ -30,7 +31,7 @@ class Cache:
     def __contains__(self, key: str) -> bool:
         return key in self.__store
 
-    def __getitem__(self, key: str) -> None | Any:
+    def __getitem__(self, key: str) -> Any | None:
         if key in self.__store:
             value, expiration_time = self.__store[key]
             now = time.time()
@@ -51,61 +52,78 @@ class Cache:
 
 
 class OpnSenseClient:
+    class Consts:
+        __INTERFACE_CONFING_URL__ = 'http://{host}/api/diagnostics/interface/getInterfaceConfig'
+        ACTIVE_WAN_ALIAS = 'Active_WAN'
+        ACTIVE_WAN_ID_ALIAS = 'Active_WAN_Id'
+        __GET_ALIAS_URL__ = 'http://{host}/api/firewall/alias_util/list/{alias}'
+        __ADD_ALIAS_URL__ = 'http://{host}/api/firewall/alias_util/add/{alias}'
+        __DELETE_ALIAS_URL__ = 'http://{host}/api/firewall/alias_util/delete/{alias}'
+
+        def __init__(self, host: str) -> None:
+            self.__host = host
+            self.INTERFACE_CONFING_URL = self.__INTERFACE_CONFING_URL__.format(
+                host=host)
+
+        def get_alias_url(self, alias: str) -> str:
+            return self.__GET_ALIAS_URL__.format(host=self.__host, alias=alias)
+
+        def add_alias_url(self, alias: str) -> str:
+            return self.__ADD_ALIAS_URL__.format(host=self.__host, alias=alias)
+
+        def delete_alias_url(self, alias: str) -> str:
+            return self.__DELETE_ALIAS_URL__.format(host=self.__host, alias=alias)
+
     class CacheKeys:
         ALL_GATEWAYS = 'allactgw'
         ACTIVE_GATEWAY_ID = 'actgwid'
         ACTIVE_GATEWAY_NAME = 'actgwname'
         ACTIVE_GATEWAY_IP = 'actgwip'
 
-    def __init__(self) -> None:
-        self.host = cfg.OpnSense.HOST
+    def __init__(self, host: str | None = None, wans: dict[str, tuple[int, str]] | None = None,
+                 key: str | None = None, secret: str | None = None) -> None:
+        self.__host = host if host else cfg.OpnSense.HOST
+        self.__wans = wans if wans else cfg.OpnSense.WANS
+        self.__key = key if key else cfg.OpnSense.KEY
+        self.__secret = secret if secret else cfg.OpnSense.SECRET
         self.__cache = Cache()
+        self.__consts = self.Consts(self.__host)
 
-    @staticmethod
-    def __make_get_request(url: str) -> requests.Response:
-        response = requests.get(url,  auth=(
-            cfg.OpnSense.KEY, cfg.OpnSense.SECRET))
+    def __make_get_request(self, url: str) -> dict[str, Any]:
+        response = requests.get(url, auth=(self.__key, self.__secret))
         response.raise_for_status()
-        return response
+        return response.json()
 
-    @staticmethod
-    def __make_post_request(url: str, body: dict[str, Any]) -> requests.Response:
-        response = requests.post(url, json=body,  auth=(
-            cfg.OpnSense.KEY, cfg.OpnSense.SECRET))
+    def __make_post_request(self, url: str, body: dict[str, Any]) -> dict[str, Any]:
+        response = requests.post(
+            url, json=body,  auth=(self.__key, self.__secret))
         response.raise_for_status()
-        return response
+        return response.json()
 
     def all_gateways(self) -> dict[str, tuple[str, int, str]]:
         if not (gateways := self.__cache[self.CacheKeys.ALL_GATEWAYS]):
-            url = cfg.OpnSense.INTERFACE_CONFING_URL.format(host=self.host)
-            response = self.__make_get_request(url)
-            rdict = response.json()
+            r = self.__make_get_request(self.__consts.INTERFACE_CONFING_URL)
             # gateways: dict[str, tuple[str, int, str]]
             gateways = {}
-            for wan, (priority, id) in cfg.OpnSense.WANS.items():
-                if wan in rdict and (ipv4 := rdict[wan]['ipv4']):
+            for wan, (priority, id) in self.__wans.items():
+                if wan in r and (ipv4 := r[wan]['ipv4']):
                     gateways[wan] = ipv4[0]['ipaddr'], priority, id
             self.__cache[self.CacheKeys.ALL_GATEWAYS] = gateways, 5
         return gateways
 
     def active_gateway(self) -> tuple[str | None, str]:
-        # get Active_WAN alias value
-        if not (ip := self.__cache[self.CacheKeys.ACTIVE_GATEWAY_IP]):
-            url = cfg.OpnSense.GET_ALIAS_URL.format(
-                host=self.host, alias=cfg.OpnSense.ACTIVE_WAN_ALIAS)
-            response = self.__make_get_request(url)
-            rdict = response.json()
-            ip = rdict['rows'][0]['ip']
-            self.__cache[self.CacheKeys.ACTIVE_GATEWAY_IP] = ip, 300
+        def get_alias_value(alias: str, cache_key: str) -> str:
+            if not (value := self.__cache[cache_key]):
+                url = self.__consts.get_alias_url(alias)
+                r = self.__make_get_request(url)
+                value = r['rows'][0]['ip']
+                self.__cache[cache_key] = value, 300
+            return value
 
-        # get Active_WAN_Id alias value
-        if not (id := self.__cache[self.CacheKeys.ACTIVE_GATEWAY_ID]):
-            url = cfg.OpnSense.GET_ALIAS_URL.format(
-                host=self.host, alias=cfg.OpnSense.ACTIVE_WAN_ID_ALIAS)
-            response = self.__make_get_request(url)
-            rdict = response.json()
-            id = rdict['rows'][0]['ip']
-            self.__cache[self.CacheKeys.ACTIVE_GATEWAY_ID] = id, 300
+        ip = get_alias_value(self.Consts.ACTIVE_WAN_ALIAS,
+                             self.CacheKeys.ACTIVE_GATEWAY_IP)
+        id = get_alias_value(self.Consts.ACTIVE_WAN_ID_ALIAS,
+                             self.CacheKeys.ACTIVE_GATEWAY_ID)
 
         if gwname := self.__cache[self.CacheKeys.ACTIVE_GATEWAY_NAME]:
             return gwname, ip
@@ -125,38 +143,36 @@ class OpnSenseClient:
     def update_active_gateway(self, wan_name: str | None = None) -> bool:
         def set_alias(alias: str, new_value: str, old_value: str) -> bool:
             # delete old entry
-            url = cfg.OpnSense.DELETE_ALIAS_URL.format(
-                host=self.host, alias=alias)
+            url = self.__consts.delete_alias_url(alias)
             r = self.__make_post_request(url, {'address': old_value})
-            if r.json()['status'] == 'failed':
+            if r['status'] == 'failed':
                 return False
 
             # add new entry
-            url = cfg.OpnSense.ADD_ALIAS_URL.format(
-                host=self.host, alias=alias)
+            url = self.__consts.add_alias_url(alias)
             r = self.__make_post_request(url, {'address': new_value})
-            if r.json()['status'] == 'failed':
+            if r['status'] == 'failed':
                 return False
 
             return True
 
         def set_active_wan(new_ip: str, old_ip: str) -> bool:
             del self.__cache[self.CacheKeys.ACTIVE_GATEWAY_IP]
-            if set_alias(cfg.OpnSense.ACTIVE_WAN_ALIAS, new_ip, old_ip):
+            if set_alias(self.__consts.ACTIVE_WAN_ALIAS, new_ip, old_ip):
                 self.__cache[self.CacheKeys.ACTIVE_GATEWAY_IP] = ip, 300
                 return True
             return False
 
         def set_active_wan_id(new_id: str, wan_name: str) -> bool:
             if not (old_id := self.__cache[self.CacheKeys.ACTIVE_GATEWAY_ID]):
-                url = cfg.OpnSense.GET_ALIAS_URL.format(
-                    host=self.host, alias=cfg.OpnSense.ACTIVE_WAN_ID_ALIAS)
-                rdict = self.__make_get_request(url).json()
-                old_id = rdict['rows'][0]['ip']
+                url = self.__consts.get_alias_url(
+                    self.__consts.ACTIVE_WAN_ID_ALIAS)
+                r = self.__make_get_request(url)
+                old_id = r['rows'][0]['ip']
 
             del self.__cache[self.CacheKeys.ACTIVE_GATEWAY_NAME]
             del self.__cache[self.CacheKeys.ACTIVE_GATEWAY_ID]
-            if set_alias(cfg.OpnSense.ACTIVE_WAN_ID_ALIAS, new_id, old_id):
+            if set_alias(self.__consts.ACTIVE_WAN_ID_ALIAS, new_id, old_id):
                 self.__cache[self.CacheKeys.ACTIVE_GATEWAY_ID] = id, 300
                 self.__cache[self.CacheKeys.ACTIVE_GATEWAY_NAME] = wan_name, 300
                 return True
@@ -174,38 +190,45 @@ class OpnSenseClient:
             return set_active_wan(ip, old_ip) and set_active_wan_id(id, wan_name)
 
         # update current active WAN IP address
-        elif old_wan and old_wan in gateways and gateways[old_wan] and (ip := gateways[old_wan][0]) != old_ip:
+        elif old_wan and old_wan in gateways and (ip := gateways[old_wan][0]) != old_ip:
             return set_active_wan(ip, old_ip)
 
         return False
 
 
 class GandiClient:
+    class Consts:
+        __MAIN_RECORD_URL__ = 'https://api.gandi.net/v5/livedns/domains/{domain}/records/%40/A'
+
+        def __init__(self, domain: str) -> None:
+            self.MAIN_RECORD_URL = self.__MAIN_RECORD_URL__.format(
+                domain=domain)
+
     class CacheKeys:
         DOMAIN_IP = 'domip'
 
-    def __init__(self) -> None:
+    def __init__(self, domain: str | None = None, apikey: str | None = None) -> None:
+        self.__domain = domain if domain else cfg.Gandi.DOMAIN
+        self.__apikey = apikey if apikey else cfg.Gandi.API_KEY
         self.__cache = Cache()
+        self.__consts = self.Consts(self.__domain)
 
-    @staticmethod
-    def __make_get_request(url: str) -> requests.Response:
+    def __make_get_request(self, url: str) -> dict[str, Any]:
         response = requests.get(
-            url, headers={'Authorization': f'Apikey {cfg.Gandi.API_KEY}'})
+            url, headers={'Authorization': f'Apikey {self.__apikey}'})
         response.raise_for_status()
-        return response
+        return response.json()
 
-    @staticmethod
-    def __make_put_request(url: str, body: dict[str, Any]) -> requests.Response:
+    def __make_put_request(self, url: str, body: dict[str, Any]) -> dict[str, Any]:
         response = requests.put(
-            url, headers={'Authorization': f'Apikey {cfg.Gandi.API_KEY}'}, json=body)
+            url, headers={'Authorization': f'Apikey {self.__apikey}'}, json=body)
         response.raise_for_status()
-        return response
+        return response.json()
 
     def domain_ip(self) -> str:
         if not (ip := self.__cache[self.CacheKeys.DOMAIN_IP]):
-            response = self.__make_get_request(cfg.Gandi.MAIN_RECORD_URL)
-            rdict = response.json()
-            ip = rdict['rrset_values'][0]
+            r = self.__make_get_request(self.__consts.MAIN_RECORD_URL)
+            ip = r['rrset_values'][0]
             self.__cache[self.CacheKeys.DOMAIN_IP] = ip, 600
         return ip
 
@@ -215,8 +238,8 @@ class GandiClient:
             'rrset_values': [ip],
             'rrset_ttl': 300
         }
-        response = self.__make_put_request(cfg.Gandi.MAIN_RECORD_URL, record)
-        success = response.json()['message'] == 'DNS Record Created'
+        r = self.__make_put_request(self.__consts.MAIN_RECORD_URL, record)
+        success = r['message'] == 'DNS Record Created'
         del self.__cache[self.CacheKeys.DOMAIN_IP]
         if success:
             self.__cache[self.CacheKeys.DOMAIN_IP] = ip, 600
@@ -234,7 +257,7 @@ def daemon(opnclient: OpnSenseClient, gandiclient: GandiClient):
                                                 key=lambda item: item[1][1])
     log.info(f'Available gateways: {available_gateways}')
     log.info(f'Current gateway: {current_gateway} --- {current_ip}')
-    log.info(f'Priority gateway: {priority_gateway} --- { priority_ip}')
+    log.info(f'Priority gateway: {priority_gateway} --- {priority_ip}')
 
     # gateway is alive
     if current_gateway in available_gateways:
@@ -264,7 +287,7 @@ def daemon(opnclient: OpnSenseClient, gandiclient: GandiClient):
 
     end_time = time.time()
     log.info(
-        f'daemon job finished succesfully in {round(end_time - start_time, 3)} seconds')
+        f'Daemon job finished succesfully in {round(end_time - start_time, 3)} seconds')
 
 
 def main():
